69347a2c4c1e6179dc74e82f8139f521e8848827
Vladislav <itochanenko@gmail.com>
2021-10-24 20:14:46 +0300

    Init Java project for MPJ library

---
+++ Lab 1/src/Main.java
@@ -0,0 +1,11 @@
+import mpi.MPI;
+
+public class Main {
+    public static void main(String[] args) {
+        MPI.Init(args);
+        int me = MPI.COMM_WORLD.Rank();
+        int size = MPI.COMM_WORLD.Size();
+        System.out.println("Hello from " + me + ". Size: " + size);
+        MPI.Finalize();
+    }
+}
